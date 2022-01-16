13c714cc0df16c2afc79c13fa8ff4614b0d2c92e
ragdoll <museopkim0214@gmail.com>
2022-01-16 16:48:26 +0900

    [#1] chore: 프로젝트 초기 설정
    Co-authored-by: kses1010 <thstkd92@gmail.com>

---
+++ src/main/java/com/cbplan/CbplanApplication.java
@@ -0,0 +1,13 @@
+package com.cbplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbplanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbplanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cbplan/CbplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbplanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
