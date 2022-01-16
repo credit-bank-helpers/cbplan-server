445feb882269e87be4571ac1d058ebe03861648e
ragdoll <museopkim0214@gmail.com>
2022-01-16 18:10:51 +0900

    [#1] chore: 초기 SQL 수정
    Co-authored-by: kses1010 <thstkd92@gmail.com>

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,24 +1,26 @@
-CREATE TABLE IF NOT EXISTS member (
-  member_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NULL,
-  email VARCHAR(255) NULL,
-  oauth_provider VARCHAR(255) NULL,
-  oauth_id VARCHAR(255) NULL,
-  oauth_email VARCHAR(255) NULL,
-  profile_image VARCHAR(2047) NULL,
-  major_credit INTEGER NULL,
-  elective_credit INTEGER NULL,
-  general_credit INTEGER NULL,
-  created_at TIMESTAMP NULL,
-  updated_at TIMESTAMP NULL,
-  major_id BIGINT NULL,
-  CONSTRAINT member_major_id_fk FOREIGN KEY (major_id) REFERENCES major (major_id)
-);
+START TRANSACTION;
 
 CREATE TABLE IF NOT EXISTS major
 (
     major_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     major_name VARCHAR(255) NULL
+);
+
+CREATE TABLE IF NOT EXISTS member (
+                                      member_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                      name VARCHAR(255) NULL,
+                                      email VARCHAR(255) NULL,
+                                      oauth_provider VARCHAR(255) NULL,
+                                      oauth_id VARCHAR(255) NULL,
+                                      oauth_email VARCHAR(255) NULL,
+                                      profile_image VARCHAR(2047) NULL,
+                                      major_credit INTEGER NULL,
+                                      elective_credit INTEGER NULL,
+                                      general_credit INTEGER NULL,
+                                      created_at TIMESTAMP NULL,
+                                      updated_at TIMESTAMP NULL,
+                                      major_id BIGINT NULL,
+                                      CONSTRAINT member_major_id_fk FOREIGN KEY (major_id) REFERENCES major (major_id)
 );
 
 CREATE TABLE IF NOT EXISTS subject
@@ -31,16 +33,16 @@ CREATE TABLE IF NOT EXISTS subject
 );
 
 CREATE TABLE IF NOT EXISTS member_major_credit (
-  member_major_credit_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  member_id BIGINT NULL,
-  major_id BIGINT NULL,
-  major_name VARCHAR NULL,
-  subject_id BIGINT NULL,
-  subject_name VARCHAR NULL,
-  institute_id BIGINT NULL,
-  institute_name VARCHAR NULL,
-  subject_credit INTEGER NULL,
-  CONSTRAINT member_major_credit_member_id_fk FOREIGN KEY (member_id) REFERENCES member (member_id)
+                                                   member_major_credit_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                                   member_id BIGINT NULL,
+                                                   major_id BIGINT NULL,
+                                                   major_name VARCHAR(255) NULL,
+                                                   subject_id BIGINT NULL,
+                                                   subject_name VARCHAR(255) NULL,
+                                                   institute_id BIGINT NULL,
+                                                   institute_name VARCHAR(255) NULL,
+                                                   subject_credit INTEGER NULL,
+                                                   CONSTRAINT member_major_credit_member_id_fk FOREIGN KEY (member_id) REFERENCES member (member_id)
 );
 
 CREATE TABLE IF NOT EXISTS institute
